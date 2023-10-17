f3c6aa87f57552469c7363c1e484b36f42fc78f5
Greg Osetrov <shadow-greg@yandex.ru>
2023-10-17 09:04:05 +0300

    all done

---
+++ Bits.cs
@@ -0,0 +1,17 @@
+﻿namespace homework; 
+
+public class Bits
+{
+    private readonly byte[] data;
+
+    public Bits(byte[] data)
+    {
+        this.data = data;
+    }
+
+    public static implicit operator Bits(long value) => new(BitConverter.GetBytes(value));
+
+    public static implicit operator Bits(int value) => new(BitConverter.GetBytes(value));
+
+    public static implicit operator Bits(byte value) => new(new[] { value });
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Реализуйте операторы неявного приведения из long, int, byt в Bits
+
+using homework;
+
+public class Program {
+    public static void Main(string[] args) {
+        const long l = 1234567890123456789;
+        Bits bitsFromLong = l; 
+
+        const int i = 123456789;
+        Bits bitsFromInt = i; 
+
+        const byte b = 255;
+        Bits bitsFromByte = b; 
+    }
+}
